ec26847de8dfb376356a0fd04958bcdce6f74813
compscirunner <compscirunner@gmail.com>
2022-07-14 14:33:59 -0400

    added support for multiple answers

--- src/quizGame/GameApp.java
+++ src/quizGame/GameApp.java
@@ -2,6 +2,7 @@ package quizGame;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GameApp {
@@ -16,6 +17,9 @@ public class GameApp {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 
+	//TODO Add Test user name
+	//TODO add date to report
+	
 	public static void main(String[] args) {
 		int count = 1;
 		int correctCount = 0;
@@ -30,10 +34,15 @@ public class GameApp {
 				String[] qA = fileInput.nextLine().split(",");
 				String question = qA[0];
 				String answer = qA[1].strip();
+				String[] answers = Arrays.copyOfRange(qA, 1, qA.length);
+				System.out.println(Arrays.toString(answers));
+				
+			
 				System.out.println(count+ ": " + question);
 				System.out.print("Enter answer: ");
 				String userAnswer = input.nextLine();
-				if(userAnswer.equalsIgnoreCase(answer)) {
+				//if((userAnswer.equalsIgnoreCase(answer))) {
+				if(Arrays.asList(answers).contains(userAnswer)) {
 					report +=  "✅ " + count + ": " + question + "\n" ;
 					correctCount++;
 				}
