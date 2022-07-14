8c300ab817c107d5792b30f69b853ed305303ce9
compscirunner <compscirunner@gmail.com>
2022-07-14 12:25:51 -0400

    questions are processed

--- src/quizGame/GameApp.java
+++ src/quizGame/GameApp.java
@@ -8,12 +8,27 @@ public class GameApp {
 
 	public static void main(String[] args) {
 		 
-		
+		Scanner input = new Scanner(System.in);
 		//File file = new File("questions.txt");
 		File file = new File("src/quizGame/questions.txt");
 		 try {
 			Scanner fileInput = new Scanner(file);
-			System.out.println(fileInput.nextLine());
+			while(fileInput.hasNextLine()) {
+				
+				String[] qA = fileInput.nextLine().split(",");
+				String question = qA[0];
+				String answer = qA[1].strip();
+				System.out.println(question);
+				System.out.print("Enter answer: ");
+				String userAnswer = input.nextLine();
+				if(userAnswer.equalsIgnoreCase(answer)) {
+					System.out.println("Great Job");
+				}
+				else {
+					System.out.println("Sorry the correct answer was: "+ answer);
+				}
+			}
+			
 			
 			
 			
