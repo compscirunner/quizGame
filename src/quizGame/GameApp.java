36753be0d26b7b98d80b593c261f7fbdcad96f7f
compscirunner <compscirunner@gmail.com>
2022-07-14 12:06:00 -0400

    linked to file

--- src/quizGame/GameApp.java
+++ src/quizGame/GameApp.java
@@ -1,9 +1,26 @@
 package quizGame;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GameApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		
+		//File file = new File("questions.txt");
+		File file = new File("src/quizGame/questions.txt");
+		 try {
+			Scanner fileInput = new Scanner(file);
+			System.out.println(fileInput.nextLine());
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
